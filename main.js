e39c1b68373464c15515c9d17906afc7db35ff5b
StungByAJellyfish <heiditucholski@gmail.com>
2026-02-05 13:10:27 -0500

    Astro Weight Calculator with Pluto option

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// Write your JavaScript code here!
+
+var planets = [ 
+        {name: 'Pluto', gravity: 0.06}, 
+        {name: 'Neptune', gravity: 1.148}, 
+        {name: 'Uranus', gravity: 0.917}, 
+        {name: 'Saturn', gravity: 1.139}, 
+        {name: 'Jupiter', gravity: 2.640}, 
+        {name: 'Mars', gravity: 0.3895}, 
+        {name: 'Moon', gravity: 0.1655}, 
+        {name: 'Earth', gravity: 1}, 
+        {name: 'Venus', gravity: 0.9032}, 
+        {name: 'Mercury', gravity: 0.377}, 
+        {name: 'Sun', gravity: 27.9} 
+    ];
